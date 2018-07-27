245edd5c982e4b6ff0e959df77f76c8b7d1f2156
Nikolai Ilushko <kolyaxil@gmail.com>
2018-07-28 01:16:14 +0300

    Learned a bit of Hapi

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ server.route({
     method: 'GET',
     path: '/{name}',
     handler: (request, h) => {
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+        request.logger.info('In handler %s', request.path)
+        return `Hello, ${encodeURIComponent(request.params.name)}!`
     }
 })
 
 const init = async  () => {
-    await server.register(require('inert'))
 
-    server.route({
-        method: 'GET', 
-        path: '/hello',
-        handler: (request, h) => {
-            return h.file('./public/hello.html');
+    await server.register({
+        plugin: require('hapi-pino'),
+        options: {
+            prettyPrint: false, 
+            logEvents: ['response']
         }
     })
 
